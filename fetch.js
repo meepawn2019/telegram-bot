5e1db16349407f96d8a2e67e9cbc3511ac672e3d
meepawn2019 <meepawn2018@gmail.com>
2022-09-23 17:44:47 +0700

    update bot

---
+++ fetch.js
@@ -0,0 +1,21 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const FormData = require('form-data');
+
+const getData = async () => {
+  // fetch data with form data and return it
+  const formData = new URLSearchParams();
+  formData.append('login', 'Demo');
+  formData.append('password', 'DemoKairete2022@');
+  const response = await fetch('https://www.kairete.net/api/auth', {
+    method: 'POST',
+    headers: {
+      'XF-Api-Key': 'Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y',
+    },
+    body: formData,
+  });
+  // const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  console.log(data);
+};
+
+getData();
--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-var FormData = require("form-data");
-var request = require("request");
-const TeleBot = require("telebot");
-const sqlite3 = require("sqlite3").verbose();
-require("dotenv").config();
+import TeleBot from "telebot";
+import sqlite3 from "sqlite3";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const db = new sqlite3.Database("./login.db");
-
-let sql = `SELECT DISTINCT Name name FROM playlists
-           ORDER BY name`;
 
 const bot = new TeleBot(TOKEN);
 
-const timeout = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
+import { open } from "sqlite";
 
-const MOCKING_DATABASE = [
-  {
-    username: "admin",
-    password: "admin",
-    tele_id: "869251272",
-    some_info: "some info",
-  },
-  {
-    username: "Demo",
-    password: "DemoKairete2022@",
-    tele_id: "869251272",
-    some_info: "some info",
-  },
-];
 
 bot.on(["/login"], async (msg) => {
   const loginCredentials = msg.text.split(" ")[1].split("||");
@@ -38,117 +17,125 @@ bot.on(["/login"], async (msg) => {
   const password = loginCredentials[1];
   // fake time consuming task
   bot.sendMessage(msg.from.id, "Checking your credentials...");
-  const formData = new FormData();
+  const formData = new URLSearchParams();
   formData.append("login", username);
   formData.append("password", password);
-  var options = {
+  const data = await fetch("https://www.kairete.net/api/auth", {
     method: "POST",
-    url: "https://www.kairete.net/api/auth",
     headers: {
       "XF-Api-Key": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
     },
-    formData: {
-      login: username,
-      password: password,
-    },
-  };
-  request(options, function (error, response) {
-    if (error) {
-      return bot.sendMessage(msg.from.id, "Login failed!, please try again");
-    }
-    let user;
-    const obj = JSON.parse(response.body);
-    console.log(obj);
-    if (obj.errors) return bot.sendMessage(msg.from.id, "Login failed!, please try again");
-    // find user in sqlite database
-    db.all(
-      "SELECT * FROM users WHERE telegram_id = ?",
-      [msg.from.id],
-      (err, rows) => {
-        if (err) {
-          console.log(err);
-          return bot.sendMessage(
-            msg.from.id,
-            "DB failed, cannot select user from database",
-          );
-        }
-        rows.forEach((row) => {
-          user = row;
-        });
-        console.log(rows);
-        // if user is not found in sqlite database
-        if (!user) {
-          // create new user in sqlite database
-          db.run(
-            "INSERT INTO users(telegram_id, user_id) VALUES(?, ?)",
-            [msg.from.id, obj.user.user_id],
-            function (err) {
-              if (err) {
-                return bot.sendMessage(
-                  msg.from.id,
-                  "DB failed, Not marked as logged in",
-                );
-              }
-              let replyMarkup = bot.inlineKeyboard([
-                [bot.inlineButton("Get data", { callback: obj.user.user_id })],
-              ]);
-              console.log(user);
-              console.log(`A row has been inserted with rowid ${this.lastID}`);
-              return bot.sendMessage(
-                msg.from.id,
-                `Welcome ${username}!, your tele_id is ${msg.from.id}}`,
-                { replyMarkup },
-              );
-            },
-          );
-        } else {
-          const params = {
-            creator_id: obj.user.user_id,
-          };
-          const searchParams = new URLSearchParams(params);
-          var options = {
-            method: "GET",
-            url: "https://www.kairete.net/api/blog-entries?" + searchParams,
-            headers: {
-              "XF-Api-Key": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
-            },
-          };
-          request(options, function (error, response) {
-            if (error) {
-              bot.sendMessage(msg.from.id, "Error");
-            }
-            console.log(response.body);
-            return bot.sendMessage(msg.from.id, response.body);
-          });        
-        }
-      },
-    );
+    body: formData,
   });
+  if (data.errors) return bot.sendMessage(msg.from.id, "Invalid credentials");
+  const db = await open({
+    filename: "./login.db",
+    driver: sqlite3.Database,
+  });
+  const dataJSON = await data.json();
+  const user = await db.get(
+    "SELECT * FROM users WHERE user_id = ? AND telegram_id = ?",
+    [dataJSON.user.user_id, msg.from.id]
+  );
+  if (user) {
+    const category = await fetch("https://kairete.net/api/blog-categories", {
+      method: "GET",
+      headers: {
+        "XF-Api-Key": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
+        "XF-Api-User": dataJSON.user.user_id,
+      },
+    });
+    if (category.errors)
+      return bot.sendMessage(msg.from.id, "Error category API");
+    const categoryJSON = await category.json();
+    let { categories } = categoryJSON;
+    categories = categories.map((category) => {
+      return {
+        id: category.category_id,
+        name: category.title,
+      };
+    });
+    const replyMarkup = bot.inlineKeyboard(
+      [[...categories.map((category) => {
+        return bot.inlineButton(category.name, { callback: JSON.stringify({categoryId: category.id, userId: dataJSON.user.user_id}) });
+      }), bot.inlineButton("All category", { callback: JSON.stringify({categoryId: -1, userId: dataJSON.user.user_id}) })]],
+    );
+    return bot.sendMessage(
+      msg.from.id,
+      `Welcome ${username}!, your tele_id is ${msg.from.id}}`,
+      { replyMarkup }
+    );
+  }
 });
 
-bot.on("callbackQuery", (msg) => {
-  console.log(msg.data);
+bot.on("callbackQuery", async (msg) => {
+  const msgData = JSON.parse(msg.data);
+  let params = {};
+  if (msgData.categoryId === -1) {
+    // get all category
+    const category = await fetch("https://kairete.net/api/blog-categories", {
+      method: "GET",
+      headers: {
+        "XF-Api-Key": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
+        "XF-Api-User": msgData.userId,
+      },
+    });
+    if (category.errors)
+      return bot.sendMessage(msg.from.id, "Error category API");
+    const categoryJSON = await category.json();
+    let { categories } = categoryJSON;
+    categories = categories.map((category) => {
+      return {
+        id: category.category_id,
+        name: category.title,
+      };
+    });
+    categories.forEach((category, index) => {
+      params[`category_ids[${index}]`] = category.id;
+    });
+  } else {
+    params["category_ids[0]"] = msgData.categoryId;
+  }
   // User message alert
-  const params = {
-    creator_id: msg.data,
-  };
   const searchParams = new URLSearchParams(params);
-  var options = {
+  console.log(searchParams);
+  const data = await fetch(`https://www.kairete.net/api/blog-entries?${searchParams}`, {
     method: "GET",
-    url: "https://www.kairete.net/api/blog-entries?" + searchParams,
     headers: {
-      "XF-Api-Key": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
+      "XF-Api-KEY": "Bj-iF2DqxqJcBEolg9H6Qjp94ekWVM1Y",
+      "XF-API-USER": 1,
     },
-  };
-  request(options, function (error, response) {
-    if (error) {
-      bot.sendMessage(msg.from.id, "Error");
-    }
-    console.log(response.body);
-    return bot.sendMessage(msg.from.id, response.body);
   });
-  // bot.sendMessage(msg.from.id, `Callback data: ${ msg.data }`);
-  // return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
+  if (data.errors) return bot.sendMessage(msg.from.id, "Error category API");
+  const dataJSON = await data.json();
+  let sendData = dataJSON["blogEntryItems"].map((item) => {
+    return {
+      title: item?.title,
+      thumbnail: item?.CoverImage?.thumbnail_url,
+      content: item?.message_plain_text,
+      url: item?.view_url,
+    };
+  });
+  // send message to user multiple message combine each object in 1 message
+  for await (const item of sendData) {
+      try {
+      const replyMarkup = bot.inlineKeyboard([
+        [bot.inlineButton("Read more", { url: item.url })],
+      ]);
+      await bot.sendPhoto(msg.from.id, item.thumbnail, {
+        caption: item.title,
+      });
+      console.log(item.title);
+      await bot.sendMessage(msg.from.id, `${item.title}
+  
+  ${item.content.slice(0, 300)}`, {
+        replyMarkup,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return bot.sendMessage("Done");
 });
 
 // Inline query
